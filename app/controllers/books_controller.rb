b22b1c7aa89f23d0a74c63831214920e1b6469f4
h-sara <115357536+h-sara@users.noreply.github.com>
2023-01-09 13:57:05 +0000

    [Fix] 細かいところの修正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,6 +2,11 @@ class BooksController < ApplicationController
   def new
   end
 
+  def index
+    @books = Book.all
+    @book = Book.new
+  end
+
   def create
     @book = Book.new(book_params)
     if @book.save
@@ -12,10 +17,7 @@ class BooksController < ApplicationController
     end
   end
 
-  def index
-    @books = Book.all
-    @book = Book.new
-  end
+
 
   def update
     @book = Book.find(params[:id])
